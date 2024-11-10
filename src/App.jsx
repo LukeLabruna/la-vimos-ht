e6127b05b526399cf4ec8fbadb3ff510173807b4
Lucas Labruna <lucaspablolabruna@gmail.com>
2024-11-10 20:47:59 -0300

    Updated app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react"
 import NavBar from "./components/NavBar/NavBar"
 import MoviesContainer from "./components/MoviesContainer/MoviesContainer"
-import moviesdb from "././assets/updatedMovies.json"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import MovieDetailContainer from "./components/MovieDetailContainer/MovieDetailContainer"
 import './App.css'
